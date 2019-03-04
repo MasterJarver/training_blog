78758a243efa985d50c757542e773a38bc588c8e
Jarver <masterlogdog@gmail.com>
2019-03-04 17:44:24 +0200

    add connection to database

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const config = require('./config');
+const bodyParser = require('body-parser'); // нужен для парсинга тела http POST запросов req.body
+const app = express(); // создание экземпляра приложения
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true})); // использование в приложении bodyParser
+const arr = ['hello', 'world', 'test'];
+app.get('/', (req, res) => res.render('index', {arr: arr})); // передача JSON с полем data и значением data, будет использоваться в шаблоне
+app.get('/create', (req, res) => res.render('create'));
+app.post('/create', (req, res) => {
+    console.log(req.body);
+    arr.push(req.body.text); // добавление строки из input в массив
+    res.redirect('/');
+}); // получанеи и вывод инфы из POST запроса
+module.exports = app;
--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-const express = require('express');
+const app = require('./app');
+const database = require('./database');
 const config = require('./config');
-const bodyParser = require('body-parser'); // нужен для парсинга тела http POST запросов req.body
-const app = express(); // создание экземпляра приложения
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true})); // использование в приложении bodyParser
-const arr = ['hello', 'world', 'test'];
-app.get('/', (req, res) => res.render('index', {arr: arr})); // передача JSON с полем data и значением data, будет использоваться в шаблоне
-app.get('/create', (req, res) => res.render('create'));
-app.post('/create', (req, res) => {
-    console.log(req.body);
-    arr.push(req.body.text); // добавление строки из input в массив
-    res.redirect('/');
-}); // получанеи и вывод инфы из POST запроса
+database()
+    .then(info => {
+        console.log(`Connected to ${info.host}:${info.port}/${info.name}`);
+    })
+    .catch(() => {
+    console.error('Unable to connect to database');
+    process.exit(1);
+    });
 app.listen(config.PORT, () => console.log('app listening on 3000 port'));
