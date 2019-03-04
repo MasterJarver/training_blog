eec367faaa55346cbe44e79262c99361596150f0
Jarver <masterlogdog@gmail.com>
2019-03-04 15:24:47 +0200

    add create page

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ const app = express();
 app.set('view engine', 'ejs');
 const data = 'hi';
 app.get('/', (req, res) => res.render('index', {data: data})); // передача JSON с полем data и значением data, будет использоваться в шаблоне
+app.get('/create', (req, res) => res.render('create'));
 app.listen(3000, () => console.log('app listening on 3000 port'));
