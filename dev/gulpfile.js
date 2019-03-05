73f22dad2542d54034df8e45bc1791ed4c7f20ff
Jarver <masterlogdog@gmail.com>
2019-03-05 15:26:12 +0200

    add task browser-sync and compile sass

--- dev/gulpfile.js
+++ dev/gulpfile.js
@@ -1,1 +1,27 @@
 const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const cssnano = require('gulp-cssnano');
+const browserSync = require('browser-sync');
+gulp.task('scss', () => {
+    return gulp
+        .src('dev/scss/**/*.scss') // будет брать все файлы в подпапках
+        .pipe(sass()) // sass компилирует исходник в css
+        .pipe(
+            autoprefixer(['last 15 versions', '> 1%', 'ie 8'], {
+                cascade: true
+            }))
+        .pipe(cssnano())
+        .pipe(gulp.dest('dist/css')); // выгрузка скомпилированного css в директорию
+});
+gulp.task('browser-sync', () => {
+    browserSync({
+        server: {
+            baseDir: 'dist'
+        },
+        notify: false
+    });
+});
+gulp.task('default', ['scss'], () => { // создание задачи для gulp
+    gulp.watch('dev/scss/**/*.scss', ['scss']);
+});
