32ca9e9504014395e57f7198387104ed14b26cdb
Jarver <masterlogdog@gmail.com>
2019-03-05 12:04:30 +0200

    select posts

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ const Post = require('./models/post');
 const app = express(); // создание экземпляра приложения
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true})); // использование в приложении bodyParser
-const arr = ['hello', 'world', 'test'];
-app.get('/', (req, res) => res.render('index', {arr: arr})); // передача JSON с полем data и значением data, будет использоваться в шаблоне
+
+app.get('/', (req, res) => {
+    Post.find({}).then(posts => {
+        res.render('index', {posts: posts});
+    });
+}); // передача JSON с полем data и значением data, будет использоваться в шаблоне
 app.get('/create', (req, res) => res.render('create'));
 app.post('/create', (req, res) => {
     const {title, body} = req.body;
