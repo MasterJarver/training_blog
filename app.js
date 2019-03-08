2bb11493c84b31350fa39380d3ef3d013b8eed71
Jarver <masterlogdog@gmail.com>
2019-03-08 15:01:33 +0200

    insert front template to main project

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser'); // нужен для парсинга тела http POST запросов req.body
 const path = require('path');
+const staticAsset = require('static-asset');
 const app = express(); // создание экземпляра приложения
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true})); // использование в приложении bodyParser
+app.use(staticAsset(path.join(__dirname, 'public'))); // подключение статической ссылки на public
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/javascripts', express.static(path.join(__dirname, 'node_modules', 'jquery', 'dist')));
 app.get('/', (req, res) => {
--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const cssnano = require('gulp-cssnano');
-const browserSync = require('browser-sync');
 const plumber = require('gulp-plumber');
 gulp.task('scss', () => {
     return gulp
@@ -13,18 +12,8 @@ gulp.task('scss', () => {
                 cascade: true
             }))
         .pipe(cssnano())
-        .pipe(gulp.dest('dist/css')) // выгрузка скомпилированного css в директорию
-        .pipe(browserSync.reload({stream: true}));
+        .pipe(gulp.dest('public/stylesheets')) // выгрузка скомпилированного css в директорию
 });
-gulp.task('browser-sync', () => {
-    browserSync({
-        server: {
-            baseDir: 'dist'
-        },
-        notify: false
-    });
-});
-gulp.task('default', ['browser-sync', 'scss'], () => { // создание задачи для gulp
+gulp.task('default', ['scss'], () => { // создание задачи для gulp
     gulp.watch('dev/scss/**/*.scss', ['scss']);
-    gulp.watch('dist/*.html', browserSync.reload);
 });
